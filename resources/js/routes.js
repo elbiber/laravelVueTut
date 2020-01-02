a1d208c94532abd2d26e4d2378a2bf2b692792e2
Willy Weise <hellmuth.willy.weise@stud.fh-luebeck.de>
2020-01-02 01:46:12 +0100

    starting ui

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,17 +1,17 @@
 import VueRouter from 'vue-router'
-import ExampleComponent from './components/ExampleComponent'
-import Example2 from './components/Example2'
+import Simulator from './components/Simulator'
+import Items from './components/Items'
 
 const routes = [
     {
         path: '/',
-        component: ExampleComponent,
-        name: 'home'
+        name: 'simulator',
+        component: Simulator
     },
     {
-        path: '/second',
-        component: Example2,
-        name: 'second'
+        path: '/items',
+        name: 'items',
+        component: Items
     }
 ]
 
